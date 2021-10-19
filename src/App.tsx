c53544484cf0a51f625b8f4e230305b18bf3f0a7
Hiago de Oliveira <hiagodeoliveiratv@gmail.com>
2021-10-19 12:19:06 -0300

    Projeto já listando as tarefas

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import * as C from './App.styles';
+import { Item } from './types/Item';
+import { ListItem } from './components/ListItem';
 
-function App() {
+const App = ()=>{
+  const [ list, setList ] = useState<Item[]>([
+    {id: 1, name: 'Comprar pão na padaria', done: true},
+    {id: 1, name: 'Comprar um bolo na padaria', done: false},
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <C.Container>
+      <C.Area>
+        <C.Header>Lista de Tarefas</C.Header>
+
+        {/* Área de adiconar nova tarefa*/}
+        {
+          list.map((item, index)=>(
+            <ListItem item={item} key={index} />
+          ))
+        }
+
+      </C.Area>
+    </C.Container>
   );
 }
 
